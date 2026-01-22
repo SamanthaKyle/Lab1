b9fa3f8236ec8b59c8d201d75a0d83a329734745
Samantha Kyle <samanthalkyle@gmail.com>
2026-01-21 19:15:06 -0500

    final submittable lab

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const btn = document.getElementById("my-button");
+const btn1 = document.getElementById("view_geoJSON");
+const btn2 = document.getElementById("view_portfolio");
 
-btn.addEventListener("click", () => {
-    // alert("You clicked me!");
-    console.log("You clicked me");
+btn1.addEventListener("click", () => {
+    // Consulted this resource:
+    // https://stackoverflow.com/questions/6303964/javascript-open-a-given-url-in-a-new-tab-by-clicking-a-button
+    window.open("./places.geojson", "_blank")
+})
+
+btn2.addEventListener("click", () => {
+    window.open("https://samanthakyle.github.io/", "_blank")
 })
